367e42c786a37943b25acc859ca0e60b922a2957
developerav <developer-av@yandex.ru>
2021-08-16 14:34:37 +0300

    enable pretty url

--- api/config/main.php
+++ api/config/main.php
@@ -36,14 +36,12 @@ return [
         'errorHandler' => [
             'errorAction' => 'error/error',
         ],
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
     ],
     'params' => $params,
 ];
